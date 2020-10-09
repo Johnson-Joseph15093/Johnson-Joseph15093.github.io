371e31688f2bbe1bcb4a3a624c7a71cfeb978292
Johnson-Joseph15093 <joh15093@gmail.com>
2020-10-09 13:47:47 -0500

    Initial Commit

--- lesson4/js/date.js
+++
@@ -1,5 +0,0 @@
-let date = new Date();
-let modified = document.lastModified;
-
-document.getElementById("copyright-year").innerHTML = date.getFullYear();
-document.getElementById("modified-date").innerHTML = modified;
