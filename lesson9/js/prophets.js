3fca4e8f542c7b6c28d2c13aa7ac7c45d211fb64
Johnson-Joseph15093 <joh15093@gmail.com>
2021-03-05 12:39:08 -0600

    Initial Commit

---
+++ lesson9/js/prophets.js
@@ -0,0 +1,31 @@
+const requestURL = 'byui-cit230.github.io/canvas-referenced/latter-day-prophets.json';
+fetch (requestURL)
+  .then(function (response) {
+      return response.json();
+  })
+  .then(function (jsonObject) {
+      console.table(jsonObject);
+      
+  const prophets = jsonObject['prophets'];
+  for (let i = 0; i < prophets.length; i++ ) {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let dob = document.createElement("p");
+    let bp = document.createElement("p");
+    let image = document.createElement("img");
+
+    h2.textContent = prophets[i].name + " " + prophets[i].lastname;
+    dob.textContent = "Date of Birth: " + prophets[i].birthdate;
+    bp.textContent = "Place of Birth: " + prophets[i].birthplace;
+    image.setAttribute("src", prophets[i].imageurl);
+    image.setAttribute("alt", (prophets[i].name + " " + prophets[i].lastname + " - " + prophets[i].order));
+
+    card.appendChild(h2);
+    card.appendChild(dob);
+    card.appendChild(bp);
+    card.appendChild(image);
+    card.setAttribute("class", "card");
+
+    document.querySelector("div.cards").appendChild(card);
+    }
+  });
