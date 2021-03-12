fb2456fb0cfb3e8b1cf4c7243d88ff250bbe423d
Johnson-Joseph15093 <joh15093@gmail.com>
2021-03-11 23:23:09 -0600

    Initial commit

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,9 +1,10 @@
-const apiURL = "https://openweathermap.org/city/5604473";
+const apiURL = "https://openweathermap.org/data/2.5/forecast?=5604473&appid=4ba8e18b879f91c8c2f3e2cf0ebf901a";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('current-temp').textContent = jsBoject.main.temp;
+    const temperature = document.getElementById('current-temp');
+    temperature.textContent = jsBoject.main.temp;
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
 const desc = jsObject.weather[0].description;
 document.getElementById('imagesrc').textContent = imagesrc;
